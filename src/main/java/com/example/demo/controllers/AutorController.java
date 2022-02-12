5da00c40c9337f1b8808ce092d17aedb600a0538
Rafael <80859966+rafael-oliveira000@users.noreply.github.com>
2022-02-12 15:07:14 -0300

    Entrega

--- src/main/java/com/example/demo/controllers/AutorController.java
+++ src/main/java/com/example/demo/controllers/AutorController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-
 @RestController
 @RequestMapping("/api/v1/autor")
 public class AutorController {
